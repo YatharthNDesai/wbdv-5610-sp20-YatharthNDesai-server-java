d7c0445c5d2ddbc4247a5eee061bdd41d5a862a0
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-01-27 22:46:25 +0530

    create user and render users update

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,36 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001377543/users';
+    var self = this;
+    function createUser(user) {
+        return fetch(self.url,{
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.json())
+
+    }
+    function findAllUsers() {
+       return fetch(self.url).then((response) =>
+            // console.log(response)
+             response.json()
+        )
+       //  let promise = fetch(self.url)
+       //  promise.then(function (response) {
+       //      console.log(response)
+       //
+       //  })
+    }
+    function findUserById(userId) {
+        return fetch(`${self.url}/${userId}`)
+            .then((response => response.json()))
+    }
+    function updateUser(userId, user) {}
+    function deleteUser(userId) {}
+}
